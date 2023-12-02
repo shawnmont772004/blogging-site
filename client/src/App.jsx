6d1272547208c30315a58a8e682f04a56d481cf7
shawnmont772004 <21b47.shawn@sjec.ac.in>
2023-12-02 17:00:46 +0530

    created routes for routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
-import {BrowserRouter as Router, Routes, Routes} from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+
+import Home from "./pages/Home.jsx"
+import SignUp from "./pages/SignUp.jsx"
+import SignIn from "./pages/SignIn.jsx"
+import About from './pages/About.jsx'
+import Profile from './pages/Profile.jsx'
 
 const App = () => {
   return (
     <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="*" element={()=>{console.log("error 404 not found")}} />
+
+
+        </Routes>
+      </Router>
+
 
     </div>
   )
