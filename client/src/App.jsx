34bde28871a53058fd5c1db04104a20af4b73efd
shawnmont772004 <21b47.shawn@sjec.ac.in>
2023-12-02 16:43:58 +0530

    installed react-router-dom for routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import {BrowserRouter as Router, Routes, Routes} from 'react-router-dom'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+
+    </div>
   )
 }
 
