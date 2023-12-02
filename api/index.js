786bc523d35829ebd51b36540353dd86ea96cf67
shawnmont772004 <21b47.shawn@sjec.ac.in>
2023-12-02 20:40:09 +0530

    connected to database using mongoose with .env for safer collabration

--- api/index.js
+++ api/index.js
@@ -1,7 +1,17 @@
 import express from 'express';
-
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 const app = express();
+
 
 app.listen(3000,()=>{
     console.log("Server is running at 3000!");
 })
+
+mongoose.connect(process.env.DB_KEY).then(()=>{
+    console.log("Connected to Database.")
+}).catch((err)=>{
+    console.log(err);
+})
+
