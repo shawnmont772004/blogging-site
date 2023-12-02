9607969ab4be96bf7027d9ea1200db3e81eebf52
shawnmont772004 <21b47.shawn@sjec.ac.in>
2023-12-02 19:03:52 +0530

    sign up page ui

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -2,7 +2,26 @@ import React from 'react'
 
 const SignUp = () => {
   return (
-    <div>SignUp</div>
+    <>
+    <div className=" flex justify-center mt-12 mb-12">
+      <div className="bg-white md:w-1/3 w-96 rounded-lg text-black flex flex-col mx-auto gap-4">
+        <h1 className="text-xl p-1 font-semibold flex  mx-auto mt-4">Sign up</h1>
+        <form className="flex mx-auto flex-col w-64 gap-4">
+          <input type="text" placeholder="first name" className="border rounded-md p-1 border-slate-800 focus:outline-none hover:bg-opacity-80"/>
+          <input type="text" placeholder="last name" className="border rounded-md p-1 border-slate-800 focus:outline-none hover:bg-opacity-80"/>
+          <input type="text" placeholder="user name" className="border rounded-md p-1 border-slate-800 focus:outline-none hover:bg-opacity-800"/>
+          <input type="email" placeholder="email" className="border rounded-md p-1 border-slate-800 focus:outline-none hover:bg-opacity-80"/>
+          <input type="tel" placeholder="phone number" className="border rounded-md p-1 border-slate-800 focus:outline-none hover:bg-opacity-80"/>
+          <input type="password" placeholder="password" className="border rounded-md p-1 border-slate-800 focus:outline-none hover:bg-opacity-80"/>
+          <button type="submit" className="text-white font-semibold text-sm  bg-slate-700 p-2 rounded-md border border-gray-300 hover:opacity-60">sign up</button>
+        </form>
+        <div className="text-sm flex mx-auto text-red-500 gap-4 mb-4">
+          <span>Have an account?</span>
+          <span className="underline">sign in</span>
+        </div>
+      </div>
+    </div>
+    </>
   )
 }
 
