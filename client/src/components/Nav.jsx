a04984eac27c4e82343939cdbe063d2e547ef9c1
shawnmont772004 <21b47.shawn@sjec.ac.in>
2023-12-02 17:36:12 +0530

    navigation bar ui with Linking routes

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 const Nav = () => {
   return (
-    <div>Nav</div>
+   <>
+    <div className="max-w-screen-2xl mx-auto">
+        <div className="p-5 flex justify-around bg-slate-800 items-center">
+            <span className="text-2xl font-bold">Mo-Blog</span>
+            <ul className="flex md:gap-24 gap-8 text-lg ">
+                <Link to="/"><li className="hover:bg-white hover:text-slate-900 p-2 border rounded-md">Home</li></Link>
+                <Link to="/about"><li className="hover:bg-white hover:text-slate-900 p-2 border rounded-md hidden md:block">About</li></Link>
+                <Link to="/sign-up"><li className="hover:bg-white hover:text-slate-900 p-2 border rounded-md">Sign-up</li></Link>    
+            </ul>
+        </div>
+    </div>
+   </>
   )
 }
 
