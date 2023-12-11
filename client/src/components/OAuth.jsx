26e2a1d6c220e1890b06b48375c209590020bc0a
shawnmont772004 <21b47.shawn@sjec.ac.in>
2023-12-11 15:25:51 +0530

    type- button to prevent default submission on oauth

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function OAuth() {
   return (
-    <button className="text-sm bg-red-600 text-white p-2 rounded-md hover:opacity-60">CONTINUE WITH GOOGLE</button>
+    <button type="button" className="text-sm bg-red-600 text-white p-2 rounded-md hover:opacity-60">CONTINUE WITH GOOGLE</button>
   )
 }
 
