0ddc7f492b02fdb5c3b98cb664482f0e78368d54
shawnmont772004 <21b47.shawn@sjec.ac.in>
2023-12-11 14:56:22 +0530

    redux-persist to preserve user state in local storage even after refresh

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,12 +1,25 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import userReducer from './user/userSlice.js'
+import {persistReducer,persistStore} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const combReducer= combineReducers({userss:userReducer});
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  version : 1
+}
+
+const persistedReducer = persistReducer(persistConfig,combReducer);
+
 export const store = configureStore({
-  reducer: {
-    userss:userReducer
-  },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) => {
     return getDefaultMiddleware({
       serializableCheck: false
     })
   }
-})
+});
+
+export const persistor = persistStore(store)
