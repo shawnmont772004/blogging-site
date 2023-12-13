493b026c5851bf2fef756da8705f516359422313
shawnmont772004 <21b47.shawn@sjec.ac.in>
2023-12-13 22:03:23 +0530

    profile pic displayed on authentication in nav over sign-in

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 const Nav = () => {
+    const { currentUser } = useSelector((state) => state.userss)
+    //if you have any pother const name besides currentUser like existtUser or newUser it wouldnt show you the profile pic
   return (
    <>
     <div className="max-w-screen-2xl mx-auto">
@@ -9,7 +12,7 @@ const Nav = () => {
             <ul className="flex md:gap-24 gap-8 text-lg ">
                 <Link to="/"><li className="hover:bg-white hover:text-slate-900 p-2 border rounded-md">Home</li></Link>
                 <Link to="/about"><li className="hover:bg-white hover:text-slate-900 p-2 border rounded-md hidden md:block">About</li></Link>
-                <Link to="/sign-in"><li className="hover:bg-white hover:text-slate-900 p-2 border rounded-md">Sign-in</li></Link>    
+                {currentUser ? (<Link to="profile"><img src={currentUser.ProfilePic} alt="profile" className="rounded-full w-16 h-16"/></Link>) : (<Link to="/sign-in"><li className="hover:bg-white hover:text-slate-900 p-2 border rounded-md">Sign-in</li></Link>   )} 
             </ul>
         </div>
     </div>
